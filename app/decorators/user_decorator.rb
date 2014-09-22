e4b5810ceaa4fd062c29e0d33601bfcc470bd8c9
shizm <satoru.yamasaki5@gmail.com>
2014-09-22 17:02:41 +0900

    デコレーターのプラグインをdraperに変更

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,5 +1,6 @@
-# coding: utf-8
-module UserDecorator
+class UserDecorator < Draper::Decorator
+  delegate_all
+
   def avatar_url
     gravatar_id = Digest::MD5::hexdigest(email).downcase
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=32"
